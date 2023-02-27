d591ffd80c5f73fd4ca35c9e65fed515159f053a
Martin Maly <martin.maly.mm@email.cz>
2023-02-27 16:51:02 +0000

    added number of observations, corrected names of statistics, do not plot cc1/2 etc when they are not available

--- script/import_serial_report.py
+++ script/import_serial_report.py
@@ -23,14 +23,18 @@ class import_serial_report(Report):
             #overallStats.append("<p>Overall statistics</p>")
             overallStats = parent.addFold(label='Overall statistics', initiallyOpen=True)
 
-            tableDiv = overallStats.addDiv(style="height:250px;width:20em;float:left;border:0px;")
+            #tableDiv = overallStats.addDiv(style="height:250px;width:20em;float:left;border:0px;")
+            tableDiv = overallStats.addDiv(style="width:20em;float:left;border:0px;")
             table = tableDiv.addTable(transpose=True)
-            table.addData(title="# Unique reflections",select='overall/n_unique')
-            table.addData(title="Completeness (%)",select='overall/completeness')
+            table.addData(title="Low resolution limit",select='overall/d_max')
+            table.addData(title="High resolution limit",select='overall/d_min')
+            table.addData(title="Number observed reflections",select='overall/n_obs')
+            table.addData(title="Number unique reflections",select='overall/n_unique')
+            table.addData(title="Completeness %",select='overall/completeness')
             table.addData(title="Multiplicity",select='overall/multiplicity')
-            table.addData(title="Mean I",select='overall/I')
-            table.addData(title="Mean I/sigma(I)",select='overall/IsigI')
-            table.addData(title="CC(1/2)",select='overall/cc')
+            table.addData(title="Mean(I)",select='overall/I')
+            table.addData(title="Mean((I)/sd(I))",select='overall/IsigI')
+            table.addData(title="Half-set correlation CC(1/2)",select='overall/cc')
             table.addData(title="CC*",select='overall/CCstar')
             table.addData(title="Rsplit",select='overall/rsplit')
             clearingDiv = overallStats.addDiv(style="clear:both;")
@@ -39,62 +43,75 @@ class import_serial_report(Report):
 
             binnedStats = parent.addFold(label='Statistics vs. resolution', initiallyOpen=True)
 
-            graph = binnedStats.addFlotGraph(title="Multiplicity and completeness", xmlnode=self.xmlnode, select="binned/bin", style="width:450px;height:300px;margin:0 auto;float:left;border:0px;" )
+            graph = binnedStats.addFlotGraph(
+                title="Multiplicity and completeness v resolution",
+                xmlnode=self.xmlnode, select="binned/bin",
+                style="width:450px;height:300px;margin:0 auto;float:left;border:0px;" )
             # graph.addData(title="Bin", select="n_bin" )
             # graph.addData(title="Resolution(A)", select="d_max")
             # graph.addData(title="Resolution(A)", select="d_min")
             graph.addData(title="Resolution(A)", select="one_over_d_min_sq") # 1 (x)
-            graph.addData(title="#UniqueReflections", select="n_unique")     # 2
-            graph.addData(title="Completeness(%)", select="completeness")    # 3
-            graph.addData(title="Multiplicity", select="multiplicity")       # 4
+            graph.addData(title="#ObservedReflections", select="n_obs")      # 2
+            graph.addData(title="#UniqueReflections", select="n_unique")     # 3
+            graph.addData(title="Completeness(%)", select="completeness")    # 4
+            graph.addData(title="Multiplicity", select="multiplicity")       # 5
             p = graph.addPlotObject()
             p.append( 'title', 'Multiplicity' )
             p.append( 'plottype', 'xy' )
             #p.append( 'xintegral', 'true' )
             p.append( 'xscale', 'oneoversqrt' )
             p.append( 'xlabel', 'Resolution (A)' )
-            l = p.append('plotline',xcol=1,ycol=4)
+            l = p.append('plotline',xcol=1,ycol=5)
             p = graph.addPlotObject()
             p.append( 'title', 'Completeness (%)' )
             p.append( 'plottype', 'xy' )
             p.append( 'xscale', 'oneoversqrt' )
             p.append( 'xlabel', 'Resolution (A)' )
-            l = p.append('plotline',xcol=1,ycol=3)
+            l = p.append('plotline',xcol=1,ycol=4)
+            p = graph.addPlotObject()
+            p.append( 'title', '#ObservedReflections' )
+            p.append( 'plottype', 'xy' )
+            p.append( 'xscale', 'oneoversqrt' )
+            p.append( 'xlabel', 'Resolution (A)' )
+            l = p.append('plotline',xcol=1,ycol=2)
             p = graph.addPlotObject()
             p.append( 'title', '#UniqueReflections' )
             p.append( 'plottype', 'xy' )
             p.append( 'xscale', 'oneoversqrt' )
             p.append( 'xlabel', 'Resolution (A)' )
-            l = p.append('plotline',xcol=1,ycol=2)
+            l = p.append('plotline',xcol=1,ycol=3)
             clearingDiv = binnedStats.addDiv(style="clear:both;")
             parent.append("<br />")
 
-            graph = binnedStats.addFlotGraph(title="Data quality indicators", xmlnode=self.xmlnode, select="binned/bin", style="width:450px;height:300px;margin:0 auto;float:left;border:0px;" )
+            graph = binnedStats.addFlotGraph(title="Data quality indicators v resolution", xmlnode=self.xmlnode, select="binned/bin", style="width:450px;height:300px;margin:0 auto;float:left;border:0px;" )
             graph.addData(title="Resolution(A)", select="one_over_d_min_sq") # 1 (x)
             # graph.addData(title="MeanI", select="I" )
-            graph.addData(title="MeanI/sigma(I)", select="IsigI" )           # 2
-            graph.addData(title="CC1/2", select="cc" )                       # 3
-            graph.addData(title="CC*", select="CCstar" )                     # 4
-            graph.addData(title="Rsplit", select="rsplit" )                  # 5
-            p = graph.addPlotObject()
-            p.append( 'title', 'Correlation coefficient CC(1/2)' )
-            p.append( 'plottype', 'xy' )
-            p.append( 'xscale', 'oneoversqrt' )
-            p.append( 'xlabel', 'Resolution (A)' )
-            l = p.append('plotline',xcol=1,ycol=3)
-            l = p.append('plotline',xcol=1,ycol=4)
+            graph.addData(title="Mn(I/sdI)", select="IsigI" )           # 2
+            if bool(self.xmlnode.xpath('overall/cc')):
+                graph.addData(title="CC1/2", select="cc" )                       # 3
+                graph.addData(title="CC*", select="CCstar" )                     # 4
+                graph.addData(title="Rsplit", select="rsplit" )                  # 5
+
+                p = graph.addPlotObject()
+                p.append( 'title', 'Half-set correlation CC(1/2)' )
+                p.append( 'plottype', 'xy' )
+                p.append( 'xscale', 'oneoversqrt' )
+                p.append( 'xlabel', 'Resolution (A)' )
+                l = p.append('plotline',xcol=1,ycol=3)
+                l = p.append('plotline',xcol=1,ycol=4)
             p = graph.addPlotObject()
             p.append( 'title', 'Mean I/sigma(I)' )
             p.append( 'plottype', 'xy' )
             p.append( 'xscale', 'oneoversqrt' )
             p.append( 'xlabel', 'Resolution (A)' )
             l = p.append('plotline',xcol=1,ycol=2)
-            p = graph.addPlotObject()
-            p.append( 'title', 'Rsplit' )
-            p.append( 'plottype', 'xy' )
-            p.append( 'xscale', 'oneoversqrt' )
-            p.append( 'xlabel', 'Resolution (A)' )
-            l = p.append('plotline',xcol=1,ycol=5)
+            if bool(self.xmlnode.xpath('overall/cc')):
+                p = graph.addPlotObject()
+                p.append( 'title', 'Rsplit' )
+                p.append( 'plottype', 'xy' )
+                p.append( 'xscale', 'oneoversqrt' )
+                p.append( 'xlabel', 'Resolution (A)' )
+                l = p.append('plotline',xcol=1,ycol=5)
             clearingDiv = binnedStats.addDiv(style="clear:both;")
             parent.append("<br />")
 
